145ea62345b3e220faad53ff472d765238a40cc2
Carl ohwen martin F. Pascual <carlohwenmartinpascual@gmail.com>
2026-05-21 22:42:56 +0800

    Create Lab18_2.php

---
+++ Lab18_2.php
@@ -0,0 +1,10 @@
+<h3>POST Method</h3>
+<form method="post" action="">
+    <label>Name:</label>
+    <input type="text" name="name">
+    <br><br>
+
+    <input type="submit" value="Submit">
+</form>
+
+<!-- In GET METHOD the data is visible in the URL, whereas in POST METHOD it is not.-->
