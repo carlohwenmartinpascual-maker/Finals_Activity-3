c28e7a302dc1586a64a10eea20910a3e0bbf88f6
Carl ohwen martin F. Pascual <carlohwenmartinpascual@gmail.com>
2026-05-21 21:56:04 +0800

    Create Lab5.php

---
+++ Lab5.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<form method="post" action="">
+    Name: <input type="text" name="name">
+    <input type="submit">
+</form>
+
+<?php
+if(isset($_POST['name'])) {
+    $name = htmlspecialchars($_POST['name']);
+    echo "Hello, " . $name;
+}
+?>
+
+</body>
+</html>
